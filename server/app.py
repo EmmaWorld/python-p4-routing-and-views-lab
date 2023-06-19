fcbf49a9a0f9acb6d7c6a46261846b9f564ff69f
Mang <mangemmanuel07@gmail.com>
2023-06-19 20:38:12 +0800

    Completed Assignment

--- server/app.py
+++ server/app.py
@@ -4,33 +4,6 @@ from flask import Flask
 
 app = Flask(__name__)
 
-
-
-
-# @app.route('/')
-# def index():
-#     return '<h1>Python Operations with Flask Routing and Views</h1>'
-
-# @app.route('/print/<parameter>')
-# def print_string(parameter):
-#     return f"The parameter is: {parameter}"
-
-# @app.route('/count/<int:parameter>')
-# def count(parameter):
-#     result=""
-#     for i in range(parameter):
-#         result+= str(i) + "\n"
-#     return result
-
-
-
-
-
-
-
-# if __name__ == '__main__':
-#     app.run(port=5555, debug=True)
-
 @app.route('/')
 def index():
     return'<h1>Python Operations with Flask Routing and Views</h1>' 
